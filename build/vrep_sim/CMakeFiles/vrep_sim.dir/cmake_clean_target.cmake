3a0996db4d59f5dda1085384805a2dd51a5ecbe0
jfang <jfang@wpi.edu>
2018-03-02 14:44:15 -0500

    ready

---
+++ build/vrep_sim/CMakeFiles/vrep_sim.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "../lib/libvrep_sim.a"
+)
---
+++ src/vrep_sim/vrep_interface/robot_sim_controller.h
@@ -0,0 +1,45 @@
+/*
+ * robot_sim_controller.h
+ *
+ *  Created on: Sep 1, 2016
+ *      Author: rdu
+ */
+
+#ifndef CONTROL_SRC_VREP_SIM_VREP_INTERFACE_ROBOT_SIM_CONTROLLER_H_
+#define CONTROL_SRC_VREP_SIM_VREP_INTERFACE_ROBOT_SIM_CONTROLLER_H_
+
+//extern "C" {
+//    #include "extApi.h"
+///*	#include "extApiCustom.h" // custom remote API functions */
+//}
+//
+//#include "common/robot_state_base.h"
+
+#include <cstdint>
+
+namespace srcl_ctrl
+{
+
+template<typename DataFromSimType, typename DataToSimType, typename RobotStateType, typename RobotCmdType>
+class RobotSimController
+{
+protected:
+	RobotSimController():ctrl_loop_count_(0){};
+	virtual ~RobotSimController(){};
+
+protected:
+	RobotStateType rs_;
+	RobotStateType est_rs_;
+	uint64_t ctrl_loop_count_;
+
+public:
+	virtual const RobotStateType& GetRobotState() { return rs_;};
+	virtual DataToSimType ConvertRobotCmdToSimCmd(const RobotCmdType& cmd) = 0;
+
+	virtual void UpdateRobotState(const DataFromSimType& data) = 0;
+	virtual RobotCmdType UpdateCtrlLoop() = 0;
+};
+
+}
+
+#endif /* CONTROL_SRC_VREP_SIM_VREP_INTERFACE_ROBOT_SIM_CONTROLLER_H_ */
